02989bb1de4e8facf371e2ab5622818ddfb6b2e7
martin-wedberg <38207441+martin-wedberg@users.noreply.github.com>
2021-02-26 18:28:28 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -88,11 +88,7 @@
 
   function displayFilms(){   
     const filmContainer = document.getElementById("filmContainer");
-    fetch('../filmData.json', {
-    headers : { 
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-       }})
+    fetch('../filmData.json')
       .then(response => response.json())
       .then(data => {
         const markup = data.map(film => 
